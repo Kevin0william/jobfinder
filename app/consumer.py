a967c62d60b286d643da266ea087b07195e18a43
kevin <kevinwilliammkd@gmail.com>
2025-11-01 19:32:48 +0100

    ajout de l'options avancer avec fichier static 5

--- app/consumer.py
+++ app/consumer.py
@@ -6,43 +6,41 @@ from asgiref.sync import sync_to_async
 
 class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
-        self.user = self.scope['user']
-        self.other_user_id = self.scope['url_route']['kwargs']['user_id']
-        self.room_name = f"chat_{min(self.user.id, self.other_user_id)}_{max(self.user.id, self.other_user_id)}"
-        self.room_group_name = self.room_name
+        self.user = self.scope["user"]
+        self.receiver_id = self.scope["url_route"]["kwargs"]["user_id"]
+        self.room_group_name = f'chat_{min(self.user.id,int(self.receiver_id))}_{max(self.user.id,int(self.receiver_id))}'
 
-        # Rejoindre le groupe
-        await self.channel_layer.group_add(self.room_group_name, self.channel_name)
+        await self.channel_layer.group_add(
+            self.room_group_name,
+            self.channel_name
+        )
         await self.accept()
 
     async def disconnect(self, close_code):
-        await self.channel_layer.group_discard(self.room_group_name, self.channel_name)
+        await self.channel_layer.group_discard(
+            self.room_group_name,
+            self.channel_name
+        )
 
     async def receive(self, text_data):
         data = json.loads(text_data)
         message = data['message']
 
-        # Sauvegarder le message dans la base
-        await self.save_message(self.user.id, self.other_user_id, message)
+        # Enregistrer le message dans la DB
+        await sync_to_async(Message.objects.create)(sender=self.user,
+                                                    receiver_id=self.receiver_id,
+                                                    message=message)
 
-        # Envoyer à tout le monde dans le groupe
+        # Envoyer à tout le groupe
         await self.channel_layer.group_send(
             self.room_group_name,
             {
                 'type': 'chat_message',
                 'message': message,
                 'sender_id': self.user.id,
+                'timestamp': str(int(Message.objects.last().date.timestamp()))
             }
         )
 
     async def chat_message(self, event):
-        await self.send(text_data=json.dumps({
-            'message': event['message'],
-            'sender_id': event['sender_id'],
-        }))
-
-    @sync_to_async
-    def save_message(self, sender_id, receiver_id, message):
-        sender = User.objects.get(id=sender_id)
-        receiver = User.objects.get(id=receiver_id)
-        Message.objects.create(sender=sender, receiver=receiver, message=message)
+        await self.send(text_data=json.dumps(event))
